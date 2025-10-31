59a1c9fce9c6975c5df3c950685ae78bdca58502
Kgomotso Nacane <kgomotsonacane@gmail.com>
2025-10-31 06:04:53 +0200

    Deployment update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src/client',
-  server: { port: 5173 },
+  root: 'src/client',   // points to folder where index.html lives
+  base: './',           // for relative paths in production
+  build: {
+    outDir: '../../dist', // so dist is at project root for Netlify
+  },
 });
